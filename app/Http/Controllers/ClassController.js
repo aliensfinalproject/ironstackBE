d4ee6b45f72b7f3224e0b51aaaf1b1fa57628bc7
Ashima Shrivastava <ashima206@gmail.com>
2016-11-30 09:52:31 -0500

    added more methods for class

--- app/Http/Controllers/ClassController.js
+++ app/Http/Controllers/ClassController.js
@@ -3,6 +3,11 @@ const Classe = use("App/Model/Classe")
 
 class ClassController {
 
+	*read(request,response){
+		let listclasses = yield Classe.all()
+		response.status(200).json(listclasses)
+
+	}
 	* addClass(request,response){
 		let user = request.authUser
 		let data = request.all()
@@ -12,6 +17,35 @@ class ClassController {
 		} else {
 			response.status(401).send("Only admins are allowed to make other users admins")
 		}
+	}
+
+	* editClass(request,response){
+		let user = request.authUser
+		let classID = request.params('classId')
+		let data = request.all()
+
+		if(user.admin==true){
+			let editClass = yield Classe.findBy(classID,'id')
+			editClass.fill(data)
+			yield editClass.save()
+			response.status(201).json(editClass)
+		} else {
+			response.status(401).send("Only admins are allowed to make other users admins")
+		}
+
+	}
+
+	* deleteClass(request,response){
+		let user = request.authUser
+		let classID = request.params('classId')
+		let data = request.all()
+		if(user.admin==true){
+			let removeClass = yield Classe.findBy(classID,'id').delete()
+			response.status(201).json(editClass)
+		} else {
+			response.status(401).send("Only admins are allowed to make other users admins")
+		}
+
 
 	}
 
