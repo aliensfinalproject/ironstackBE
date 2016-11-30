bf194caaf3527fbcd735a7841708b6b0b49133d7
Ashima Shrivastava <ashima206@gmail.com>
2016-11-29 20:13:36 -0500

    added users and token table

---
+++ database/migrations/1480468121201_create_users_table.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UsersTableSchema extends Schema {
+
+  up () {
+    this.create('users', table => {
+      table.increments()
+      table.string('firstName')
+      table.string('lastName')
+      table.string('username', 80).notNullable().unique()
+      table.string('email', 254).notNullable().unique()
+      table.string('password').notNullable()
+      table.boolean('admin').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+
+}
+
+module.exports = UsersTableSchema
