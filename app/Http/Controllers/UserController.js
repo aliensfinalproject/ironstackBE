5b1f37ed22cffcfad1f11a530f17582864f3a81f
Ashima Shrivastava <ashima206@gmail.com>
2016-11-29 21:22:01 -0500

    addeduser controller

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,6 +1,36 @@
 'use strict'
+const User = use("App/Model/User")
+const Hash = use('Hash')
+
 
 class UserController {
+	* signUp(request,response){
+		let data = request.all()
+		data.password = yield Hash.make(data.password)
+		try {
+			let user = yield User.create(data)
+			response.status(201).json(user)
+		} catch(e){
+			response.status(400).send(e.message)
+		}
+
+	}
+
+	* login(request,response){
+		let data = request.only('userName','password')
+		let user = yield User.findBy('userName',data.userName)
+		try{
+			const token = yield request.auth.attempt(data.userName,data.password)
+			user.access_token = token
+			response.status(200).send(user)
+		} catch(e){
+			response.status(401).unauthorized({error: e.message})
+		}
+
+	}
+
+
+
 
 }
 
