39c357ccad0c4e45784d4d9ecfad827eb7160c85
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-02 10:32:04 -0500

    created assignment table, model, controller

---
+++ app/Http/Controllers/AssignmentController.js
@@ -0,0 +1,71 @@
+'use strict'
+const Assignment = use('App/Model/Assignment')
+
+class AssignmentController {
+
+  * create (request, response) {
+    let user = request.authUser
+    if (user.admin) {
+      let data = request.all()
+      let assignment = yield Assignment.create(data)
+      response.status(201).json(assignment)
+    } else {
+      response.status(401).json({text: 'User not authorized'})
+    }
+
+  }
+
+  * index (request, response) {
+    let assignments = yield Assignment.query().where('enabled', 1).fetch()
+    if (assignments) {
+      response.status(200).json(assignments)
+    } else {
+      response.status(401).json({text: 'Assignment not enabled'})
+    }
+
+  }
+
+  * show (request, response) {
+    let assignmentId = request.param('id')
+    let assignment = yield Assignment.findBy('id', assignmentId)
+    response.status(200).json(assignment)
+  }
+
+  * update (request, response) {
+    let user = request.authUser
+    if (user.admin){
+      let assignmentId = request.param('id')
+      let assignment = yield Assignment.findBy('id', assignmentId)
+      let data = request.all()
+      if (assignment) {
+        assignment.fill(data)
+        yield assignment.save()
+        response.status(201).json(assignment)
+      } else {
+        response.json({text: 'Assignment not found'})
+      }
+    } else {
+      response.json({text: 'User not authorized'})
+    }
+
+  }
+
+  * delete (request, response) {
+    let user = request.authUser
+    if (user.admin) {
+      let assignmentId = request.param('id')
+      let assignment = yield Assignment.findBy('id', assignmentId)
+      if (assignment) {
+        yield assignment.delete()
+        response.json({text: 'Assignment has been deleted'})
+      } else {
+        response.json({text: 'Assignment not found'})
+      }
+    } else {
+      response.json({text: 'User not authorized'})
+    }
+  }
+
+}
+
+module.exports = AssignmentController
---
+++ database/migrations/1480629712843_assignments.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class AssignmentsTableSchema extends Schema {
+
+  up () {
+    this.create('assignments', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string('title')
+      table.string('description')
+      table.integer('week')
+      table.integer('class_id')
+      table.foreign('class_id').references('classes.id')
+      table.boolean('enabled').defaultTo(false)
+    })
+  }
+
+  down () {
+    this.drop('assignments')
+  }
+
+}
+
+module.exports = AssignmentsTableSchema
