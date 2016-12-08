85dac1d4de6fc460dc9ef47cf7e3858a749e6271
Ashima Shrivastava <ashima206@gmail.com>
2016-12-08 12:35:28 -0500

    added console to test

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -22,13 +22,14 @@ class UserController {
 			let reqdUser = yield User.findBy('id',userID)
 			let userpostswrapper = yield Post.query().where('user_id',userID).fetch()
 			let userposts = userpostswrapper.value()
-			console.log(userposts)
 			for(let i=0; i<userposts.length;i++){
 				let usercommentswrapper = yield Comment.query().where('user_id',userID).fetch()
-				console.log(usercommentswrapper)
+				
 				let usercomments = userpostswrapper.value()
 				console.log(usercomments)
 				for(let j=0; j<usercomments.length;j++){
+					console.log('hi')
+
 					yield usercomments[j].delete()
 				}
 				yield userposts.delete()
