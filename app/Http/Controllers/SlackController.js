a20ece0109d868b5962d1ec12393c144ae774a63
Ashima Shrivastava <ashima206@gmail.com>
2016-12-10 14:53:30 -0500

    trying slack

--- app/Http/Controllers/SlackController.js
+++ app/Http/Controllers/SlackController.js
@@ -7,6 +7,12 @@ class SlackController {
 
    * listener (request, response) {
    	console.log(request)
+   	let slackrequest = request.only('token','user_name','text','trigger_word')
+   	let category = slackrequest.trigger_word == '#Question' ? 'question' : 'status'
+   	let data = {"title": slackrequest.text, "category": category, "user_id": 1, "class_id":3}
+   	let slackPost = yield Post.create(data)
+   	console.log(slackPost)
+
       //  let user = request.authUser
       //  let classId = request.param('id')
       //  let data = request.only("title", "content","category")
